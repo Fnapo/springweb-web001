37762aeb1352946d1b570563246d5ae1d1796942
Francisco Sola Porcuna <fran_sopo@hotmail.com>
2026-02-17 10:07:25 +0100

    first commit

---
+++ src/main/java/com/config/spring/springweb_web001/SpringwebWeb001Application.java
@@ -0,0 +1,13 @@
+package com.config.spring.springweb_web001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebWeb001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebWeb001Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/config/spring/springweb_web001/SpringwebWeb001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config.spring.springweb_web001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebWeb001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
